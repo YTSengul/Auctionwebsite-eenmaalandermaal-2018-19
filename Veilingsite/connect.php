2dfc80239bbcde3b110f80f21d58229e3bf5d8db
YTSengul <YT.Sengul@student.han.nl>
2018-11-23 10:37:40 +0100

    DB Connectie

---
+++ Veilingsite/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$hostname = 'mssql2.iprject.icasites.nl';
+$databasename = 'iproject4';
+
+try {
+    $db = new PDO("sqlsrv:Server=$hostname;Database=$databasename;ConnectionPooling=0", "iproject4","F5H8b3Jqdg");
+} catch (PDOException $e) {
+    echo("Connectie met de database mislukt. Activeer de 'getMessage' een regel hieronder om de foutmelding te lezen.");
+    //echo $e->getMessage();
+}
+
+?>
