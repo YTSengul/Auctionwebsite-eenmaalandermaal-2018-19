dc64ec837d6d1649a9a48f2eed5ac0bb6df7914c
YTSengul <YT.Sengul@student.han.nl>
2018-11-25 02:18:12 +0100

    home

--- Veilingsite/header.php
+++ Veilingsite/header.php
@@ -1,9 +1,9 @@
 <?php
-include_once "connect.php";
+//include_once "connect.php";
 ?>
 
 <!doctype html>
-<html class="no-js" lang="en" dir="ltr">
+<html class="" lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -27,5 +27,39 @@ include_once "connect.php";
             </div>
         </div>
     </div>
-</div>
-</body>
+
+    <div class="top-nav">
+        <div class="large-12 cell">
+            <ul class="dropdown menu" data-dropdown-menu>
+                <li>
+                    <a href="#">Rubriek 1</a>
+                    <ul class="menu vertical">
+                        <li><a href="#">One</a></li>
+                        <li><a href="#">Two</a></li>
+                        <li><a href="#">Three</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Rubriek 2</a></li>
+                <li><a href="#">Rubriek 3</a></li>
+                <li><a href="#">Rubriek 4</a></li>
+                <li><a href="#">Rubriek 5</a></li>
+                <li><a href="#">Rubriek 6</a></li>
+                <li><a href="#">Rubriek 7</a></li>
+                <li><a href="#">Rubriek 8</a></li>
+                <li><a href="#">Meer</a>
+                    <ul class="menu vertical">
+                        <li><a href="#">One</a></li>
+                        <li><a href="#">Two</a></li>
+                        <li><a href="#">Three</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+
