a6c15070db2bc8ceadfd9211d3ec2a0263ce3caf
Stefan oude Lohuis <SJ.oudeLohuis@student.han.nl>
2018-11-26 13:46:03 +0100

    bassis website

--- Veilingsite/components/header.php
+++ Veilingsite/components/header.php
@@ -1,53 +1,32 @@
-<?php
-include_once "connect.php";
-?>
-
-<!doctype html>
-<html class="" lang="nl" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EenmaalAndermaal</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-</head>
-
-<body>
-<div class="grid-container">
-    <div class="grid-x grid-padding-x">
-        <div class="medium-12 cell">
-            <div class="header">
-                <h1>EenmaalAndermaal</h1>
-                <ul class="menu align-right">
-                    <li><a href="#">Inloggen</a></li>
-                    <li><p>of</p></li>
-                    <li><a href="#">Registreren</a></li>
-                </ul>
-            </div>
-        </div>
+<div class="grid-x grid-padding-x">
+  <div class="medium-12 cell">
+    <div class="header">
+      <h1>Eenmaal Andermaal</h1>
+      <ul class="menu align-right">
+        <li><a href="#">login</a></li>
+        <li><p>of</p></li>
+        <li><a href="#">Registreer</a></li>
+      </ul>
     </div>
-
-    <div class="top-nav">
-        <div class="medium-12 cell">
-            <ul class="dropdown menu" data-dropdown-menu>
-                <li><a href="#">Rubriek 1</a></li>
-                <li><a href="#">Rubriek 2</a></li>
-                <li><a href="#">Rubriek 3</a></li>
-                <li><a href="#">Rubriek 4</a></li>
-                <li><a href="#">Rubriek 5</a></li>
-                <li><a href="#">Rubriek 6</a></li>
-                <li><a href="#">Rubriek 7</a></li>
-                <li><a href="#">Meer</a>
-                    <ul class="menu vertical">
-                        <li><a href="#">One</a></li>
-                        <li><a href="#">Two</a></li>
-                        <li><a href="#">Three</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <script>
-        $(document).foundation();
-    </script>
+  </div>
+</div>
+<div class="top-nav">
+  <div class="medium-12 cell">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li><a href="#">Rubriek 1</a></li>
+      <li><a href="#">Rubriek 2</a></li>
+      <li><a href="#">Rubriek 3</a></li>
+      <li><a href="#">Rubriek 4</a></li>
+      <li><a href="#">Rubriek 5</a></li>
+      <li><a href="#">Rubriek 6</a></li>
+      <li><a href="#">Rubriek 7</a></li>
+      <li><a href="#">Meer</a>
+        <ul class="menu vertical">
+          <li><a href="#">One</a></li>
+          <li><a href="#">Two</a></li>
+          <li><a href="#">Three</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
--- Veilingsite/login.php
+++ Veilingsite/login.php
@@ -1,29 +1,49 @@
 <?php
-include_once "components/header.php"
+include_once "components/connect.php";
 ?>
-<body>
-    <div class='grid-container grid-x'>
+
+<!doctype html>
+<html class="" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EenmaalAndermaal</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+
+  <body>
+    <div class="grid-container">
+
+      <!-- Include the header into the page -->
+      <?php include_once 'components/header.php'; ?>
+      <!-- ________________________________ -->
+
+      <div class='grid-container grid-x'>
+
         <div class="cell large-4">
-            <h5>Inloggen</h5>
-            <form>
-                <label>Gebruikersnaam </label>
-                <input type='text' placeholder='Gebruikersnaam'>
-                <label>Wachtwoord</label>
-                <input type='password' placeholder='Wachtwoord'>
-            </form>
+          Log IN
+          <form>
+            <label>Gebruikersnaam </label>
+            <input type='text' placeholder='Gebruikersnaam'>
+            <label>Wachtwoord</label>
+            <input type='password' placeholder='Wachtwoord'>
+          </form>
         </div>
 
-        <h5>Nieuw bij EenmaalAndermaal?</h5>
-        <h5>Registreren</h5>
         <form class='cell large-4'>
-            <label>Gebruikersnaam</label>
-            <input type="text" placeholder="Gebruikersnaam">
-            <label>Wachtwoord</label>
-            <input type="password" placeholder="Wachtwoord">
-            <label>Herhaal Wachtwoord</label>
-            <input type="password" placeholder="Herhaal wachtwoord">
-        </form> 
+          Bent U nieuw?
+          <label>Gebruikersnaam</label>
+          <input type="text" placeholder="Gebruikersnaam">
+          <label>Wachtwoord</label>
+          <input type="password" placeholder="Wachtwoord">
+          <label>Herhaal Wachtwoord</label>
+          <input type="password" placeholder="Herhaal wachtwoord">
+        </form>
+      </div>
+
+    <?php include "components/scripts.html"; ?>
     </div>
-    <?php include "scripts.html"?>
-</body>
+  </body>
 </html>
