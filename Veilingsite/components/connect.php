7e47334b660d325e0f9a1ee2e10622f6a37fcfc2
YTSengul <YT.Sengul@student.han.nl>
2018-11-26 15:24:53 +0100

    .

--- Veilingsite/components/connect.php
+++ Veilingsite/components/connect.php
@@ -6,7 +6,7 @@ $databasename = 'iproject4';
 try {
     $db = new PDO("sqlsrv:Server=$hostname;Database=$databasename;ConnectionPooling=0", "iproject4","F5H8b3Jqdg");
 } catch (PDOException $e) {
-    echo("Connectie met de database mislukt. Activeer de 'getMessage' een regel hieronder om de foutmelding te lezen.");
+    //echo("Connectie met de database mislukt. Activeer de 'getMessage' een regel hieronder om de foutmelding te lezen.");
     //echo $e->getMessage();
 }
 
