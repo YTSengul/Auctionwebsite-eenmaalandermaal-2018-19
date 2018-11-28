93e366d244833afedc2814d68297e175a2e01731
YTSengul <YT.Sengul@student.han.nl>
2018-11-28 12:03:00 +0100

    s

--- Veilingsite/components/header.php
+++ Veilingsite/components/header.php
@@ -1,16 +1,36 @@
-<div class="grid-x">
-  <div class="medium-12 cell">
-    <div class="header">
-      <h1><a href="index.php">Eenmaal Andermaal</a></h1>
-      <ul class="menu align-right">
-        <li><a href="login.php">Inloggen</a></li>
-        <li><p>|</p></li>
-        <li><a href="registreren.php">Registreren</a></li>
-      </ul>
-    </div>
-  </div>
+<div class="medium-12 cell header">
+    <h1>Eenmaal Andermaal</h1>
+    <ul class="menu align-right">
+        <?php if (!isset($_SESSION['ingelogde_gebruiker'])) {
+            echo '<li ><a href = "login.php" > Inloggen</a ></li >
+        <li ><p >|</p ></li >
+        <li ><a href = "registreren.php" > Registreren</a ></li >';
+        } else {
+            echo '<li ><a href = "logout.php" > uitloggen</a ></li >';
+        }
+        ?>
+    </ul>
 </div>
 
-<?php
-  include_once "rubriekenBoom.php";
-?>
+<div class="grid-x grid-padding-x">
+    <div class="medium-12 cell top-nav">
+        <div class="medium-12 cell">
+            <ul class="dropdown menu" data-dropdown-menu>
+                <li><a href="#">Rubriek 1</a></li>
+                <li><a href="#">Rubriek 2</a></li>
+                <li><a href="#">Rubriek 3</a></li>
+                <li><a href="#">Rubriek 4</a></li>
+                <li><a href="#">Rubriek 5</a></li>
+                <li><a href="#">Rubriek 6</a></li>
+                <li><a href="#">Rubriek 7</a></li>
+                <li><a href="#">Meer</a>
+                    <ul class="menu vertical">
+                        <li><a href="#">One</a></li>
+                        <li><a href="#">Two</a></li>
+                        <li><a href="#">Three</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
