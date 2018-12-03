9b131aa16b89fe7daaccd360ebe1eaf22ba07773
YTSengul <YT.Sengul@student.han.nl>
2018-12-03 14:32:46 +0100

    oke

--- Veilingsite/registreren.php
+++ Veilingsite/registreren.php
@@ -10,7 +10,7 @@ $formulier_validation = $valid;
 $gebruikersnaam_validation = $valid;
 $emailadres_validation = $valid;
 $wachtwoord_validation = $valid;
-// $db->setAttribute(constant('PDO::SQLSRV_ATTR_DIRECT_QUERY'), true);
+// $dbh->setAttribute(constant('PDO::SQLSRV_ATTR_DIRECT_QUERY'), true);
 
 
 if (isset($_POST["registreer"])) {
@@ -86,99 +86,106 @@ $aantal_landen = count($sql_landen_data);
             <h2 class="registreren_titel">Registreren</h2>
         </div>
     </div>
-    <form action="registreren.php" method="POST">
-        <div class="grid-x grid-padding-x">
-            <div class="medium-12 large-12 float-center cell">
-                <label>Gebruikersnaam
-                    <input <?php if ($gebruikersnaam_validation == $invalid) {
-                        echo 'class="is-invalid-input"';
-                    } ?> name="gebruikersnaam" type="text" placeholder="Uw gebruikersnaam" required>
-                </label>
-                <?php if ($gebruikersnaam_validation == $invalid) {
-                    echo '<span class="form-error is-visible" id="exemple2Error">Deze gebruikersnaam bestaat al.</span>';
-                } ?>
-                <label>Wachtwoord
-                    <input <?php if ($wachtwoord_validation == $invalid) {
-                        echo 'class="is-invalid-input"';
-                    } ?> name="wachtwoord1" type="password" placeholder="Wachtwoord" required>
-                </label>
-                <label>Herhaal wachtwoord
-                    <input <?php if ($wachtwoord_validation == $invalid) {
-                        echo 'class="is-invalid-input"';
-                    } ?> name="wachtwoord2" type="password" placeholder="Herhaal wachtwoord" required>
-                    <?php if ($wachtwoord_validation == $invalid) {
-                        echo '<span class="form-error is-visible" id="exemple2Error">Wachtwoorden komen niet overeen.</span>';
+    <div class="grid-x grid-padding-x">
+        <div class="medium-12 large-12 float-center cell">
+            <form action="registreren.php" method="POST">
+                <div class="medium-12 large-12 float-center cell registreer-box">
+                    <div class="medium-12 large-12 cell">
+                        <h4>Accountsgegevens</h4>
+                    </div>
+                    <label>Gebruikersnaam
+                        <input <?php if ($gebruikersnaam_validation == $invalid) {
+                            echo 'class="is-invalid-input"';
+                        } ?> name="gebruikersnaam" type="text" placeholder="Uw gebruikersnaam" required>
+                    </label>
+                    <?php if ($gebruikersnaam_validation == $invalid) {
+                        echo '<span class="form-error is-visible" id="exemple2Error">Deze gebruikersnaam bestaat al.</span>';
                     } ?>
-                </label>
-                <label>Voornaam
-                    <input name="voornaam" type="text" placeholder="Uw voornaam" required>
-                </label>
-                <label>Achternaam
-                    <input name="achternaam" type="text" placeholder="Uw achternaam" required>
-                </label>
-                <label>Adresregel1
-                    <input name="adresregel1" type="text" placeholder="Uw adresregel1" required>
-                </label>
-                <label>Adresregel2
-                    <input name="adresregel2" type="text" placeholder="Uw adresregel2">
-                </label>
-            </div>
-            <div class="medium-6 small-6 cell">
-                <label>Plaatsnaam
-                    <input name="plaatsnaam" type="text" placeholder="Uw plaatsnaam" required>
-                </label>
-            </div>
-            <div class="medium-6 small-6 cell">
-                <label>Postcode
-                    <input name="postcode" type="text" placeholder="Uw postcode" required>
-                </label>
-            </div>
-            <div class="medium-12 cell">
-                <label>Selecteer je land
-                    <select name="land">
-                        <?php
-                        foreach ($sql_landen_data as $landen_head) {
-                            foreach ($landen_head as $land) {
-                                if ($land == 'Nederland'){
-                                    echo '<option value="' . $land . '" selected="selected" >' . $land . '</option>';
-                                } else {
-                                    echo '<option value="' . $land . '">' . $land . '</option>';
+                    <label>Wachtwoord
+                        <input <?php if ($wachtwoord_validation == $invalid) {
+                            echo 'class="is-invalid-input"';
+                        } ?> name="wachtwoord1" type="password" placeholder="Wachtwoord" required>
+                    </label>
+                    <label>Herhaal wachtwoord
+                        <input <?php if ($wachtwoord_validation == $invalid) {
+                            echo 'class="is-invalid-input"';
+                        } ?> name="wachtwoord2" type="password" placeholder="Herhaal wachtwoord" required>
+                        <?php if ($wachtwoord_validation == $invalid) {
+                            echo '<span class="form-error is-visible" id="exemple2Error">Wachtwoorden komen niet overeen.</span>';
+                        } ?>
+                    </label>
+                </div>
+                <hr class="registreer-hr">
+                <div class="medium-12 large-12 float-center cell registreer-box">
+                    <div class="medium-12 large-12 cell">
+                        <h4>Persoonsgegevens</h4>
+                    </div>
+                    <label>Voornaam
+                        <input name="voornaam" type="text" placeholder="Uw voornaam" required>
+                    </label>
+                    <label>Achternaam
+                        <input name="achternaam" type="text" placeholder="Uw achternaam" required>
+                    </label>
+                    <label>Adresregel1
+                        <input name="adresregel1" type="text" placeholder="Uw adresregel1" required>
+                    </label>
+                    <label>Adresregel2
+                        <input name="adresregel2" type="text" placeholder="Uw adresregel2">
+                    </label>
+                    <div class="medium-6 small-6 cell">
+                        <label>Plaatsnaam
+                            <input name="plaatsnaam" type="text" placeholder="Uw plaatsnaam" required>
+                        </label>
+                    </div>
+                    <div class="medium-6 small-6 cell">
+                        <label>Postcode
+                            <input name="postcode" type="text" placeholder="Uw postcode" required>
+                        </label>
+                    </div>
+                    <label>Selecteer je land
+                        <select name="land">
+                            <?php
+                            foreach ($sql_landen_data as $landen_head) {
+                                foreach ($landen_head as $land) {
+                                    if ($land == 'Nederland') {
+                                        echo '<option value="' . $land . '" selected="selected" >' . $land . '</option>';
+                                    } else {
+                                        echo '<option value="' . $land . '">' . $land . '</option>';
+                                    }
                                 }
                             }
-                        }
-                        ?>
-                    </select>
-                </label>
-                <label>Geboortedatum
-                    <input name="geboortedatum" type="date" required>
-                </label>
-                <label>E-mailadres
-                    <input <?php if ($emailadres_validation == $invalid) {
-                        echo 'class="is-invalid-input"';
-                    } ?> name="emailadres" type="email" placeholder="Uw E-mailadres" required>
-                </label>
-                <?php if ($emailadres_validation == $invalid) {
-                    echo '<span class="form-error is-visible" id="exemple2Error">Er bestaat al een account met deze emailadres.</span>';
-                } ?>
-                <label>Veiligheidsvraag
-                    <select name="veiligheidsvraag" required>
-                        <option value="1">Wat is de naam van je eerste huisdier?</option>
-                        <option value="2">Op welk basisschool heb je gezeten?</option>
-                        <option value="3">Wat is de meisjesnaam van je moeder?</option>
-                    </select>
-                </label>
-                <label>Antwoord
-                    <input name="antwoord_op_veiligheidsvraag" type="text"
-                           placeholder="Uw antwoord op de veiligheidsvraag" required>
-                </label>
-                <input type="submit" value="Registreer" name="registreer" class="button expanded float-right">
-            </div>
+                            ?>
+                        </select>
+                    </label>
+                    <label>Geboortedatum
+                        <input name="geboortedatum" type="date" required>
+                    </label>
+                    <label>E-mailadres
+                        <input <?php if ($emailadres_validation == $invalid) {
+                            echo 'class="is-invalid-input"';
+                        } ?> name="emailadres" type="email" placeholder="Uw E-mailadres" required>
+                    </label>
+                    <?php if ($emailadres_validation == $invalid) {
+                        echo '<span class="form-error is-visible" id="exemple2Error">Er bestaat al een account met deze emailadres.</span>';
+                    } ?>
+                    <label>Veiligheidsvraag
+                        <select name="veiligheidsvraag" required>
+                            <option value="1">Wat is de naam van je eerste huisdier?</option>
+                            <option value="2">Op welk basisschool heb je gezeten?</option>
+                            <option value="3">Wat is de meisjesnaam van je moeder?</option>
+                        </select>
+                    </label>
+                    <label>Antwoord
+                        <input name="antwoord_op_veiligheidsvraag" type="text"
+                               placeholder="Uw antwoord op de veiligheidsvraag" required>
+                    </label>
+                </div>
+                <input type="submit" value="Registreer" name="registreer" class="registreer-button button expanded ">
+            </form>
         </div>
-    </form>
-
+    </div>
 </div>
 
-</body>
 <?php
 
 if (isset($_POST["registreer"]) && $formulier_validation == $valid) {
@@ -186,7 +193,11 @@ if (isset($_POST["registreer"]) && $formulier_validation == $valid) {
     $voornaam = $_POST["voornaam"];
     $achternaam = $_POST["achternaam"];
     $adresregel1 = $_POST["adresregel1"];
-    $adresregel2 = $_POST["adresregel2"];
+    if (!$_POST["adresregel2"]) {
+        $adresregel2 = '';
+    } else {
+        $adresregel2 = $_POST["adresregel2"];
+    }
     $postcode = $_POST["postcode"];
     $plaatsnaam = $_POST["plaatsnaam"];
     $land = $_POST["land"];
@@ -198,25 +209,25 @@ if (isset($_POST["registreer"]) && $formulier_validation == $valid) {
 
     $wachtwoord1_hashed = password_hash($wachtwoord1, PASSWORD_DEFAULT);
 
-    /*
-    print($gebruikersnaam) . "<br>";
-    print($voornaam) . "<br>";
-    print($achternaam) . "<br>";
-    print($adresregel1) . "<br>";
-    print($adresregel2) . "<br>";
-    print($postcode) . "<br>";
-    print($plaatsnaam) . "<br>";
-    print($land) . "<br>";
-    print($geboortedatum) . "<br>";
-    print($emailadres) . "<br>";
-    print($wachtwoord1) . "<br>";
-    print($veiligheidsvraag) . "<br>";
-    print($antwoord_op_veiligheidsvraag) . "<br>";
-    */
+
+//    print($gebruikersnaam) . "<br>";
+//    print($voornaam) . "<br>";
+//    print($achternaam) . "<br>";
+//    print($adresregel1) . "<br>";
+//    print($adresregel2) . "<br>";
+//    print($postcode) . "<br>";
+//    print($plaatsnaam) . "<br>";
+//    print($land) . "<br>";
+//    print($geboortedatum) . "<br>";
+//    print($emailadres) . "<br>";
+//    print($wachtwoord1) . "<br>";
+//    print($veiligheidsvraag) . "<br>";
+//    print($antwoord_op_veiligheidsvraag) . "<br>";
+
 
     $sql_registreer = "insert into gebruiker ([gebruikersnaam], [voornaam], [achternaam], [adresregel1], [adresregel2], [postcode], [plaatsnaam], [land], [datum], [mailbox], [wachtwoord], [vraagnummer], [antwoordtekst]) values (:gebruikersnaam, :voornaam, :achternaam, :adresregel1, :adresregel2, :postcode, :plaatsnaam, :land, :geboortedatum, :emailadres, :wachtwoord1, :veiligheidsvraag, :antwoord_op_veiligheidsvraag)";
 
-    $stmt = $db->prepare($sql_registreer);
+    $stmt = $dbh->prepare($sql_registreer);
 
     if ($stmt) {
         $stmt->bindParam(":gebruikersnaam", $gebruikersnaam, PDO::PARAM_STR);
@@ -232,18 +243,15 @@ if (isset($_POST["registreer"]) && $formulier_validation == $valid) {
         $stmt->bindParam(":wachtwoord1", $wachtwoord1_hashed, PDO::PARAM_STR);
         $stmt->bindParam(":veiligheidsvraag", $veiligheidsvraag, PDO::PARAM_STR);
         $stmt->bindParam(":antwoord_op_veiligheidsvraag", $antwoord_op_veiligheidsvraag, PDO::PARAM_STR);
-        $gebruiker_registreren = $stmt->execute();
+
 
         try {
-            if ($gebruiker_registreren) {
-                /*echo "<br>succesvol toegevoegd aan database";*/
-                header('location:login.php?registratie=true');
-            } else {
-                /*echo "<br>niet toegevoegd aan database";*/
-            }
-
+            $gebruiker_registreren = $stmt->execute();
+            echo "<br>succesvol toegevoegd aan database";
+            header('location:login.php?registratie=true');
         } catch (PDOException $e) {
-            echo $e->getMessage();
+            echo "Controleer uw ingevulde gegevens";
+            //echo $e->getMessage();
         }
     }
 }
@@ -252,3 +260,6 @@ if (isset($_POST["registreer"]) && $formulier_validation == $valid) {
 ?>
 
 <?php include "components/scripts.html"; ?>
+
+</body>
+</html>
