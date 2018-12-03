1444f09c7c413c140290de5edc2b68c9bcf07615
Stefan oude Lohuis <SJ.oudeLohuis@student.han.nl>
2018-12-03 11:33:23 +0100

    Header + Rubrieken boom in header af

--- Veilingsite/components/header.php
+++ Veilingsite/components/header.php
@@ -1,71 +1,56 @@
-
-<div class="grid-x">
-    <div class="medium-12 cell header">
-        <h1>Eenmaal Andermaal</h1>
-        <ul class="menu align-right">
-            <?php if (!isset($_SESSION['ingelogde_gebruiker'])) {
-                echo '<li ><a href = "login.php" > Inloggen</a ></li >
-        <li ><p >|</p ></li >
-        <li ><a href = "registreren.php" > Registreren</a ></li >';
-            } else {
-                echo '<li ><a href = "logout.php" > uitloggen</a ></li >';
-            }
-            ?>
-        </ul>
-    </div>
-</div>
-
-<!-- NOTE: This is the off-canvas menu that appears when you click on the hamburger menu on smaller screens. Everything in the `.off-canvas` div belongs in `src/layouts/default.html`. Copy this section into the `default.html` file and remove it from this file.  -->
-<div class="off-canvas ecommerce-header-off-canvas position-left" id="ecommerce-header" data-off-canvas>
-
-    <!-- Close button -->
-    <button class="menu-icon float-right opened-icon" type="button" data-toggle="ecommerce-header"></button>
-
-    <ul class="vertical menu">
-        <li class="main-nav-link"><a href="#">Category 1</a></li>
-    </ul>
-
-    <hr>
-
-    <!-- Menu -->
-    <ul class="menu vertical">
-        <li><a href="#">Help</a></li>
-        <li><a href="#">Order Status</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">My Account</a></li>
-    </ul>
-
-</div>
-
-<!-- NOTE: This is the header menu that appears at the top of your site. -->
-<div class="off-canvas-content data-off-canvas-content">
-
-    <div class="ecommerce-header show-for-large">
-        <div class="row align-justify align-middle">
-            <div class="shrink column">
-                <ul class="vertical medium-horizontal menu">
-                    <li class="main-nav-link"><a href="categories.html">Category 1</a></li>
-                    <li class="main-nav-link"><a href="#">Category 2</a></li>
-                    <li class="main-nav-link"><a href="why.html">Category 3</a></li>
-                    <li class="main-nav-link"><a href="build.html">Category 4</a></li>
-                    <li class="main-nav-link"><a href="#">Category 5</a></li>
-                    <li class="main-nav-link"><a href="#">Category 6</a></li>
-                    <li class="main-nav-link"><a href="#">Category 7</a></li>
-                    <li class="main-nav-link"><a href="#">Category 8</a></li>
+<div class="header hide-for-small-only">
+    <div class="grid-container noPadding">
+        <div class="grid-x">
+            <div class="medium-12 cell header">
+                <h1><a href="index.php">Eenmaal Andermaal</a></h1>
+                <ul class="menu align-right">
+                    <?php if (!isset($_SESSION['ingelogde_gebruiker'])) {
+                  echo '<li ><a class="specialheader" href = "login.php" > Inloggen</a ></li >
+                        <li ><p >of</p ></li >
+                        <li ><a class="specialheader" href = "registreren.php" > Registreren</a ></li >';
+              } else {
+                  echo '<li ><a class="specialheader" href = "logout.php" > uitloggen</a ></li >';
+              }
+              ?>
                 </ul>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="ecommerce-header-mobile hide-for-large">
-        <div class="ecommerce-header-mobile-left">
+<?php include_once "rubriekenBoom.php"; ?>
+
+<div class="off-canvas-content data-off-canvas-content">
+    <div class="ecommerce-header-mobile hide-for-medium">
+        <div class="ecommerce-header-mobile-left centerText aaa">
+            <h1 class="noPadding alalalala">EenmaalAndermaal</h1>
             <button class="menu-icon" type="button" data-toggle="ecommerce-header"></button>
         </div>
     </div>
 </div>
 
+<div class="off-canvas ecommerce-header-off-canvas position-left" id="ecommerce-header" data-off-canvas>
 
+    <!-- Close button -->
+    <button class="close-button" aria-label="Close menu" type="button" data-close>
+        <span aria-hidden="true">&times;</span>
+    </button>
 
-<?php
+    <ul class="vertical menu">
+        <li><a class="specialsomething" href="#">EenmaalAndermaal</a></li>
+    </ul>
 
-?>
+    <hr>
+
+    <ul class="vertical menu">
+        <?php if (!isset($_SESSION['ingelogde_gebruiker'])) {
+        echo '<li ><a class="specialsomething" href = "login.php"> Inloggen</a ></li>
+              <li ><a class="specialsomething" href = "registreren.php"> Registreren</a></li >';
+    } else {
+        echo '<li ><a class="specialsomething" href = "logout.php" > uitloggen</a ></li >';
+    }
+    ?>
+        <li><a class="specialsomething" href="#">Rubriekenboom</a></li>
+    </ul>
+
+</div>
--- Veilingsite/components/rubriekenBoom.php
+++ Veilingsite/components/rubriekenBoom.php
@@ -6,13 +6,13 @@
     while($hoofdRubriek = $hoofdRubrieken->fetch()){
 
       echo "<div class='cell medium-6 large-3 rubriekSpacing'>";
-      echo "<li><a class='hoofdRubriek' href='#'><b>$hoofdRubriek[Rubrieknaam]</b></a></li>";
+      echo "<li><a class='hoofdRubriek specialsomething' href='#'><b>$hoofdRubriek[Rubrieknaam]</b></a></li>";
       echo "<hr>";
 
       $subRubrieken = $dbh->query("SELECT TOP 3 Rubrieknaam FROM Rubriek WHERE Rubriek = '$hoofdRubriek[Rubrieknummer]' ORDER BY Volgnr, Rubrieknaam ASC");
       while($subRubriek = $subRubrieken->fetch()){
 
-        echo "<li><a class='subRubriek' href='#'>$subRubriek[Rubrieknaam]</a></li>";
+        echo "<li><a class='subRubriek specialsomething' href='#'>$subRubriek[Rubrieknaam]</a></li>";
       }
       echo "</div>";
     }
@@ -24,30 +24,23 @@
     $hoofdRubrieken = $dbh->query("SELECT top 7 Rubrieknaam, Rubrieknummer FROM Rubriek WHERE Rubriek = -1 ORDER BY Volgnr, Rubrieknaam ASC");
     while($hoofdRubriek = $hoofdRubrieken->fetch()){
 
-      echo "<li><a href='#'>$hoofdRubriek[Rubrieknaam]</a>";
-      echo "<ul class='menu'>";
-
-      $subRubrieken = $dbh->query("SELECT Rubrieknaam FROM Rubriek WHERE Rubriek = '$hoofdRubriek[Rubrieknummer]' ORDER BY Volgnr, Rubrieknaam ASC");
-      while($subRubriek = $subRubrieken->fetch()){
-
-        echo "<li><a href'#'>$subRubriek[Rubrieknaam]</a></li>";
-      }
-      echo "</ul></li>";
+      echo "<li class='spacingHeaderRubrieken noPadding noMargins'><a class='marginAndPaddingA' href='#'>$hoofdRubriek[Rubrieknaam]</a></li>";
     }
   }
 ?>
-
-<div class="top-nav">
-  <div class="medium-12 cell">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <?php rubriekenHeader(); ?>
-      <li><a href="#">Meer</a>
-        <ul class="menu MegaMenu">
-          <div class="grid-x grid-margin-x colourAndBoxMegaMenu">
-            <?php rubriekenBoom(); ?>
-          </div>
-        </ul>
-      </li>
-    </ul>
-  </div>
+<div class="grid-container noPadding colourHeader">
+    <div class="grid-x hide-for-small-only">
+        <div class="medium-12 cell">
+            <ul class="dropdown menu centerText" data-dropdown-menu>
+                <?php rubriekenHeader(); ?>
+                <li class="spacingHeaderRubrieken noPadding noMargins test"><a class="marginAndPaddingA" href="#">Meer</a>
+                    <ul class="menu MegaMenu noBorder">
+                        <div class="grid-x grid-margin-x colourAndBoxMegaMenu">
+                            <?php rubriekenBoom(); ?>
+                        </div>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
 </div>
