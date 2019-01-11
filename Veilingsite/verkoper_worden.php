7e10aabf9a8c514226453ff657a789b80ed84fa1
Stefan oude Lohuis <SJ.oudeLohuis@student.han.nl>
2019-01-11 10:14:49 +0100

    Start verkoper_worden pagina

---
+++ Veilingsite/verkoper_worden.php
@@ -0,0 +1,22 @@
+<?php
+    include_once "components/connect.php";
+    include_once "components/meta.php";
+?>
+
+<body>
+
+    <?php include_once "components/header.php"; ?>
+
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="cell">
+                <p>Hello world</p>
+            </div>
+        </div>
+    </div>
+
+    <?php include "components/scripts.html"; ?>
+
+</body>
+
+</html>
